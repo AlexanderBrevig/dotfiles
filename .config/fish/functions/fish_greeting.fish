cdd40f883f036eb8f7fd877edb2ddf0eb8c37233
Alexander Brevig <alexanderbrevig@gmail.com>
2021-02-15 00:05:46 +0100

    Automatically show tasks based on time for work/study/work/home/play

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,3 +1,26 @@
 function fish_greeting
-cal
+    set hour (date +"%S")
+
+    if test $hour -ge 7
+        and test $hour -lt 18
+        task context work
+    end
+    if test $hour -ge 11
+        and test $hour -lt 12
+        task context study
+    end
+    if test $hour -ge 12
+        and test $hour -lt 18
+        task context work
+    end
+    if test $hour -ge 18
+        and test $hour -lt 21
+        task context home
+    end
+    if test $hour -ge 21
+        or test $hour -lt 7
+        task context play
+    end
+    clear
+    task next
 end
