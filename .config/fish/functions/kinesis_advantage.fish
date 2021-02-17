597fab8560c3d28c190b3af5be9bd1a00110897d
Alexander Brevig <alexanderbrevig@gmail.com>
2021-02-17 08:20:22 +0100

    Fix keyboard default and create fish functions for setting laptop/kinesis

---
+++ .config/fish/functions/kinesis_advantage.fish
@@ -0,0 +1,4 @@
+function kinesis_advantage
+setxkbmap -option
+setxkbmap -model pc105 -layout us,no -variant ,winkeys -option grp:shifts_toggle -option caps:escape
+end
---
+++ .config/fish/functions/laptop.fish
@@ -0,0 +1,4 @@
+function laptop
+setxkbmap -option
+setxkbmap -model pc105 -layout no -variant winkeys
+end
