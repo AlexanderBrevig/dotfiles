3d67d066b886eb4aaf1d48ceb86182b0e31b4bef
Alexander Brevig <alexanderbrevig@gmail.com>
2021-11-27 17:38:02 +0100

    remove taskwarrior

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,26 +1,2 @@
 function fish_greeting
-    set hour (date +"%H")
-
-    if test $hour -ge 7
-        and test $hour -lt 18
-        task context work
-    end
-    if test $hour -ge 11
-        and test $hour -lt 12
-        task context study
-    end
-    if test $hour -ge 12
-        and test $hour -lt 18
-        task context work
-    end
-    if test $hour -ge 18
-        and test $hour -lt 21
-        task context home
-    end
-    if test $hour -ge 21
-        or test $hour -lt 7
-        task context play
-    end
-    clear
-    task next
 end
--- setup_taskwarrior.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# copy over *pem files from server
-task config taskd.certificate -- ~/.task/alexander_brevig.cert.pem
-task config taskd.key         -- ~/.task/alexander_brevig.key.pem
-task config taskd.ca          -- ~/.task/ca.cert.pem
