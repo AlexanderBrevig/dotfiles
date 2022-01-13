6dd471af12f9532b326638f5cdefd369ebf9bde8
Alexander Brevig <alexanderbrevig@gmail.com>
2022-01-13 19:30:23 +0100

    sway.sh init sway machines, not complete WIP

---
+++ sway.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+yay -Syu
+# Absolutely basics
+yay -S base-devel coreutils cmake
+# Terminal essentials
+yay -S alacritty kitty neovim fish fish-fzf fish-z-git
+# Tool essentials
+yay -S bat exa ripgrep stow tldr jq
+# Browsers
+yay -S firefox firefox-ublock-origin chromium google-chrome
+# Theme
+yay -S gruvbox-dark-gtk gruvbox-dark-icons-gtk
+# Applications
+yay -S bitwarden bitwarden-cli blender kicad kicad-library kicad-library-3d mattermost mattermost-desktop
+
+stow sway -t ~
+stow waybar -t ~
+stow kitty -t ~
+stow fish -t ~
+stow starship -t ~
+stow nvim -t ~
+stow git -t ~
