147171ba21a50beb80fd90acef6c22918c3cb5d6
Alexander Brevig <alexander.brevig@entur.org>
2022-03-21 15:36:10 +0100

    sway: add emacs and peco

--- sway.sh
+++ sway.sh
@@ -5,12 +5,13 @@ yay -S base-devel coreutils cmake
 # Terminal essentials
 yay -S alacritty kitty neovim fish fish-fzf fish-z-git
 # Tool essentials
-yay -S bat exa ripgrep stow tldr jq
+yay -S bat exa ripgrep stow tldr jq peco
 # Browsers
 yay -S firefox firefox-ublock-origin chromium google-chrome
 # Theme
 yay -S gruvbox-dark-gtk gruvbox-dark-icons-gtk
 # Applications
+yay -S emacs-git
 yay -S bitwarden bitwarden-cli blender kicad kicad-library kicad-library-3d mattermost mattermost-desktop
 
 stow sway -t ~
@@ -21,3 +22,4 @@ stow starship -t ~
 stow nvim -t ~
 stow git -t ~
 stow doom -t ~
+stow peco -t ~
